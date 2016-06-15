fdc981cff02809e4d1f84f88c93d9bbaee1a401d
Cristian Dan <dan93cristian@yahoo.com>
2016-06-15 13:35:25 +0300

    drop target on config job list  [wip]

--- ConfigJobList.js
+++ ConfigJobList.js
@@ -1,15 +1,16 @@
 import React, { PropTypes } from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import ConfiguredJob from './ConfiguredJob'
+import { DropTarget } from 'react-dnd';
 
-// scoate onDrag
-const ConfigJobList = ({ jobs, onMove }) => {
+
+const ConfigJobList = ({ jobs, onMove, onDrop }) => {
     return (<div style={{
             width: '20%',
             height: '70%',
             display: 'flex',
             flexWrap: 'wrap',
-            backgroundColor: 'blue'
+            backgroundColor: 'yellow'
           }}>
     {jobs.map(job =>
       <ConfiguredJob name={job.name} id={job.id} key={job.id} onMove={(dragIndex, hoverIndex) => onMove(dragIndex, hoverIndex)}/>
@@ -19,7 +20,8 @@ const ConfigJobList = ({ jobs, onMove }) => {
 
 ConfigJobList.propTypes = {
   jobs: ImmutablePropTypes.list.isRequired,
-  onMove: PropTypes.func.isRequired
+  onMove: PropTypes.func.isRequired,
+  onDrop: PropTypes.func.isRequired
 }
 
 module.exports = ConfigJobList;
